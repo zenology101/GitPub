bb246598f1d9295773f49b820dde4df02f5db96d
Zen Ahmed <zenahmed994@gmail.com>
2021-10-08 19:18:36 -0400

    final commit

---
+++ models/food.js
@@ -0,0 +1,39 @@
+const food = [
+    {
+      name: '(Req, Rescargot)',
+      price: 12,
+      image: 'https://i.imgur.com/BRgv2rz.jpg'
+    },
+    {
+      name: 'Nulltimate Nachos',
+      price: 10,
+      image: 'https://i.imgur.com/vKRbSHN.jpg'
+    },
+    {
+      name: 'Split() Pea Soup',
+      price: 1,
+      image: 'https://i.imgur.com/qd9jheG.jpg'
+    },
+    {
+      name: 'Curly Fries',
+      price: 11,
+      image: 'https://i.imgur.com/lEQ1AdY.jpg'
+    },
+    {
+      name: 'Garlic NaaN',
+      price: 202,
+      image: 'https://i.imgur.com/UEx7cYk.jpg'
+    },
+    {
+      name: 'Baby Got BackEnd Ribs',
+      price: 2,
+      image: 'https://i.imgur.com/XbRMQ3g.jpg'
+    },
+    {
+      name: 'Git Pull Pork Sandwich',
+      price: 43,
+      image: 'https://i.imgur.com/QZW3gJg.jpg'
+    }
+  ]
+
+  module.exports = food; 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 //import library 
 const express = require("express");
 const drinks = require("./models/drinks");
+const food = require("./models/food");
 
 //creating the application object 
 const app = express();
@@ -24,6 +25,15 @@ app.get("/drinks/:id", (req, res) => {
     res.render("drinks_show.ejs", {drink: drinks[req.params.id]})
 })
 
+//food route index  
+app.get('/food', (req,res) => {
+    res.render('food_index.ejs', {food:food})
+})
+
+//food route show
+app.get('/food/:id', (req,res) => {
+    res.render('food_show.ejs', {food:food[req.params.id]})
+})
 
 
 //listen route for server  
